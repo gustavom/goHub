1792b1d333f6a343baef5823eb799a7f1f0250a4
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-07-22 19:19:55 -0300

    repo detais

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import api from '../../services/api';
 
 // import { Container } from './styles';
 
-export default function Repository({ match }) {
-  return (
-    <h1>Repository: {decodeURIComponent(match.params.repository)}</h1>
-  );
-}
+export default class Repository extends Component{
+
+  async componentDidMount(){
+    const {match} = this.props;
+
+    const repoName = decodeURIComponent(match.params.repository);
+
+    // usando desestruturação
+    // as posiçoes do array abaixo correspondem as possições que o array
+    // da promise retorna.
+    // Promise.all - serve para fazer multiplas requisições, sem esperar
+    // uma acabar para começar outra
+    const [ repository, issues ] = await Promise.all([
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`)
+    ]);
+
+    console.log(repository);
+    console.log(issues);
+  }
+
+  render(){
+    return (
+      <h1>Repository:</h1>
+    );
+  }
+};
