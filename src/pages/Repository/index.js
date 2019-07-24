32a1c49218dd012b679cdf1b4b0c43bccec99412
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-07-24 11:28:09 -0300

    add link from router and component container

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import api from '../../services/api';
 
-import { Loading } from './styles';
+
+import Container from '../../components/Container';
+import { Loading, Owner } from './styles';
 
 export default class Repository extends Component{
   static propTypes = {
@@ -55,7 +58,14 @@ export default class Repository extends Component{
     }
 
     return (
-      <h1>Repository:</h1>
+      <Container>
+        <Owner>
+          <Link to="/">Voltar aos repositórios</Link>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login}/>
+          <h1>{repository.name}</h1>
+          <p>{repository.description}</p>
+        </Owner>
+      </Container>
     );
   }
 };
