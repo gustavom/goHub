b4df777fd1b7cf8992b23d4066f64c142dc5e66c
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-07-24 14:34:09 -0300

    Updates

---
+++ precache-manifest.1c75316ac6e81845d8bc18f7870f7fb3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bfed770cfc238a4211fc745e1468840",
+    "url": "/goHub/index.html"
+  },
+  {
+    "revision": "6d6db5a9823487699764",
+    "url": "/goHub/static/js/2.fba58c57.chunk.js"
+  },
+  {
+    "revision": "dd1f8b6ba4c9a2cbc346",
+    "url": "/goHub/static/js/main.9def9d85.chunk.js"
+  },
+  {
+    "revision": "3e2791c0ae6950b50450",
+    "url": "/goHub/static/js/runtime~main.bb720c03.js"
+  }
+]);
