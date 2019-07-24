6b98fe2987559dd746e14c9abd7200657754de95
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-07-24 11:16:03 -0300

    loading repo component style

---
+++ src/pages/Repository/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Loading = styled.div`
+  color: #fff;
+  font-size: 30px;
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
